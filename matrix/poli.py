eff1cc502e53aa658fc4b0f478f65f8b27a2a75d
AkiraDemenech <guiakira.demenechmori@gmail.com>
2022-03-13 21:34:44 -0300

    Descartes e Huat
    + Bolzano-Cauchy

--- matrix/poli.py
+++ matrix/poli.py
@@ -1,3 +1,20 @@
+def f (coef, x):
+	d = len(coef)
+	c = y = False
+	p = True
+
+	while c < d:
+		try:
+			y += p * coef[c]
+		except KeyError: # se for um dicionário		
+			d = max(coef) + 1
+		c += 1
+		p *= x
+
+
+	return y
+
+
 def sinais (coef):
 
 	d = len(coef)
@@ -18,6 +35,64 @@ def sinais (coef):
 
 	return t		
 
-print(sinais([-2, -9, 3, -5, 1]))
-print(sinais([7, -5, 2, -3, 6, 1]))
-print(sinais({0: 11, 1: -13, 2: 5, 4: 6, 5: -8, 6: 1}))
+def inverter (coef):	
+
+	c = False
+	d = len(coef)
+	invertido = [c] * d
+
+	while c < d:
+		try:
+			invertido[c] = coef[c] * (1 - (2 * (c % 2)))
+		except KeyError:	
+			d = max(coef) + 1
+			inv = {}
+			for e in range(c):
+				inv[e] = invertido[e]
+			invertido = inv
+		c += 1
+
+	return invertido	
+
+def descartes (coef):	
+
+	return sinais(coef), sinais(inverter(coef))
+
+def bolzano (coef, a, b):	
+	return f(coef, a) * f(coef, b) <= 0
+
+def huat (coef, k = None):	
+
+	if k == None:
+
+		c = 1
+		d = len(coef) - 1
+	#	h = False
+
+		while c < d:
+		#	print(c)
+			if huat(coef, c):
+				return c				
+			#	h = True
+			c += 1	
+		return None	
+		
+	
+	try:
+		m = coef[k - 1] * coef[k + 1]		
+	except Exception:	
+		m = False
+
+	try:
+		n = coef[k] ** 2
+	except Exception:		
+		n = False
+
+	return n <= m	
+
+a = [-2, -9, 3, -5, 1]
+print(huat(a), descartes(a))
+b = [7, -5, 2, -3, 6, 1]
+print(huat(b), descartes(b))
+c = {0: 11, 1: -13, 2: 5, 4: 6, 5: -8, 6: 1}
+print(huat(c), descartes(c))
